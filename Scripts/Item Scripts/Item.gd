0fc1b10200986ad7a2528da5216ccd25727e6f94
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-06 18:21:32 -0400

    items

--- Scripts/Item Scripts/Item.gd
+++ Scripts/Item Scripts/Item.gd
@@ -1,12 +1,7 @@
-extends Node2D
+extends Resource
 
+class_name Item
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@export var stackSize : int
+@export var sprite : Texture2D
+@export var name : String
