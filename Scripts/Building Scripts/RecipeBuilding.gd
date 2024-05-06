918444351156c58e0c0d0f3c37e9704c6828c410
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-06 19:54:53 -0400

    Update RecipeBuilding.gd

--- Scripts/Building Scripts/RecipeBuilding.gd
+++ Scripts/Building Scripts/RecipeBuilding.gd
@@ -6,8 +6,11 @@ var selectedRecipe : Recipe
 
 var recipes : Array[Recipe]
 
-var inputInventory : Dictionary
-var outputInventory : Dictionary
+var inputInventoryItems : Array[Item]
+var inputInventoryAmounts : Array[int]
+
+var outputInventoryItems : Array[Item]
+var outputInventoryAmounts : Array[int]
 
 var inProgress = false
 
@@ -17,8 +20,11 @@ func _ready():
 var timeElapsed = 0
 func _process(delta):
 	
-	print(inputInventory)
-	print(outputInventory)
+	print(inputInventoryItems)
+	print(inputInventoryAmounts)
+	
+	print(outputInventoryItems)
+	print(outputInventoryAmounts)
 	
 	#If we are currently crafting then either add to elapsed time or finish crafting if time is up.
 	if inProgress:
@@ -38,13 +44,17 @@ func selectRecipe(recipeIndex : int):
 	#HAVE TO MOVE WHAT WAS IN INVENTORY TO THE PLAYERS INVENTORY
 	# OR KEEP THE ITEMS IF YOU SELECT A RECIPE THAT TAKES SAME ITEMS
 	# CANT JUST CLEAR THE INVENTORY, THIS IS TEMPORARY SOLUTION
-	inputInventory.clear()
-	for item in selectedRecipe.inputs:
-		inputInventory[item] = 0
+	inputInventoryItems.clear()
+	inputInventoryAmounts.clear()
+	for item in selectedRecipe.inputItems:
+		inputInventoryItems.append(item)
+		inputInventoryAmounts.append(0)
 		
-	outputInventory.clear()
-	for item in selectedRecipe.outputs:
-		outputInventory[item] = 0
+	outputInventoryItems.clear()
+	outputInventoryAmounts.clear()
+	for item in selectedRecipe.outputItems:
+		outputInventoryItems.append(item)
+		outputInventoryAmounts.append(0)
 
 func startCraft():
 	inProgress = true
@@ -56,13 +66,14 @@ func endCraft():
 	
 func addItems(newItem : Item, count : int):
 	#Add items into the building.
-	for item in inputInventory:
-		inputInventory[item] = inputInventory[item] + count
+	for i in len(inputInventoryItems):
+		if inputInventoryItems[i] == newItem:
+			inputInventoryAmounts[i] = inputInventoryAmounts[i] + count
 	
 func enoughResources() -> bool:
 	#Checks if there are enough resources in inputInventory to satisfy the recipe one time
-	for item in inputInventory:
-		if inputInventory[item] < selectedRecipe.inputs[item]:
+	for i in len(inputInventoryItems):
+		if inputInventoryAmounts[i] < selectedRecipe.inputsAmounts[i]:
 			return false
 	return true
 	
