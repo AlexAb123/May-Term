aca896ddf016a8b5057c67dbb264f9a19fea0a2c
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-06 21:09:18 -0400

    Furnace and Items

--- Scripts/Recipe Scripts/Recipe.gd
+++ Scripts/Recipe Scripts/Recipe.gd
@@ -4,10 +4,12 @@ class_name Recipe
 
 @export var name : String
 
+@export var craftingTime : float
+
+@export_group("Inputs")
 @export var inputItems : Array[Item]
 @export var inputAmounts : Array[int]
 
+@export_group("Outputs")
 @export var outputItems : Array[Item]
 @export var outputAmounts : Array[int]
-
-@export var craftingTime : float
