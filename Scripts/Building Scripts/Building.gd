108539f98b0674f5ae88c3f0b28348659538ef7b
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-06 20:31:03 -0400

    Buildings

--- Scripts/Building Scripts/Building.gd
+++ Scripts/Building Scripts/Building.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 class_name Building
 
-var buildTime
-var cost
+@export var buildTime : int
+@export var size : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
