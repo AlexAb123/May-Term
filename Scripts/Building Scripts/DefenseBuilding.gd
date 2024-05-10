6ac132b865ae8238eec356261620a7664853e319
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-10 12:57:25 -0400

    push

--- Scripts/Building Scripts/DefenseBuilding.gd
+++ Scripts/Building Scripts/DefenseBuilding.gd
@@ -1,3 +1,26 @@
 extends Building
 
 class_name DefenseBuilding
+
+@export var health: int
+@export var damage: int
+@export var fire_rate: float
+@export var range: float
+
+
+
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+func _on_detection_area_body_entered(body):
+	hasTarget = true
+
+
+func _on_detection_area_body_exited(body):
+	hasTarget = false
