02d0f289f9fb5d3121270c741b94f56a7ddc1ea5
Cy Rosenberg <43608584+Herefersomepennys@users.noreply.github.com>
2024-05-10 12:53:36 -0400

    renaming and targetid

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 @onready var animated_sprite = get_node("AnimatedSprite2D")
-var hasTarget;
+
+var speed: int
+var damage: int
+var health: int
+var armor: int
+var hasTarget = false
+var targets: Array[int]
+var curr: Node
+
 
 func _on_detection_area_body_entered(body):
-	hasTarget = true
+	print(body.name)
+	if "Building" in body.name:
+		var temp = []
+		#currTargets = get_node("Tower").get_overlapping_bodies()
+		hasTarget = true
 
 
 func _on_detection_area_body_exited(body):
