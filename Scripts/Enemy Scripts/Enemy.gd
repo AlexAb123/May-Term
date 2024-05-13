a40917a965dd8abcd97246e48275cf43e78d8945
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-13 13:22:21 -0400

    c

--- Scripts/Enemy Scripts/Enemy.gd
+++ Scripts/Enemy Scripts/Enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite = get_node("AnimatedSprite2D")
 
-@export var speed: int
+@export var speed: int = 50
 @export var damage: int
 @export var health: int
 @export var armor: int
@@ -9,21 +9,25 @@ var hasTarget = false
 var targets: Array[int]
 var curr: Node
 
+@onready var player: Player = owner.get_node("Player")
 
-func _on_detection_area_body_entered(body):
-	print(body.name)
-	if "Building" in body.name:
-		var temp = []
-		#currTargets = get_node("Tower").get_overlapping_bodies()
-		hasTarget = true
+#
+#func _on_detection_area_body_entered(body):
+	#print(body.name)
+	#if "Building" in body.name:
+		#var temp = []
+		##currTargets = get_node("Tower").get_overlapping_bodies()
+		#hasTarget = true
 
 
-func _on_detection_area_body_exited(body):
-	hasTarget = false
+#func _on_detection_area_body_exited(body):
+	#hasTarget = false
 
 func _physics_process(delta):
-	if hasTarget:
-		print("TARGET")
-	else:
-		pass
+	#if hasTarget:
+		#print("TARGET")
+	#else:
+		#pass
+	velocity = (player.global_position - global_position).normalized()*speed
+	move_and_slide()
 		
