dd10348898b1ddb102a2d0684938ff30ab3a2d9d
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-05-24 14:33:49 -0400

    hominh

--- Scripts/Building Scripts/RangedDefenseBuilding.gd
+++ Scripts/Building Scripts/RangedDefenseBuilding.gd
@@ -5,6 +5,7 @@ class_name RangedDefenseBuilding
 @export var projectile: PackedScene
 
 func attack():
+	
 	var proj: Projectile = projectile.instantiate()
 	proj.start_position = global_position + Vector2(8,8)
 	proj.direction = (target.global_position - proj.start_position).normalized()
--- Scripts/Projectile Scripts/HomingProjectile.gd
+++ Scripts/Projectile Scripts/HomingProjectile.gd
@@ -2,8 +2,7 @@ extends Projectile
 class_name HomingProjectile
 var target: Enemy
 func _physics_process(delta: float):
-	if is_instance_valid(target) and target is Enemy:
-		look_at(target.global_position)
+	if is_instance_valid(target):
+		direction = target.global_position - global_position
+		velocity = direction.normalized() * speed
 	super(delta)
-
- 
